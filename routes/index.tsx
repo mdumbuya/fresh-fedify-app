c9c7fe9cd54bc5841a7abea75217b92288eefc98
Moses Dumbuya <moses2004112@hotmail.com>
2024-08-24 16:49:19 +0200

    Setup of basic layout

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,25 +1,14 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import Layout from "../components/Layout.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <Layout isLoggedIn={true}>
+      <div class="mt-10 px-5 mx-auto flex max-w-screen-md flex-col justify-center">
+      <div class="mx-auto text-center">
+        <h1 class="text-2xl font-bold mb-5">Nice you're logged In!</h1>
+        <a href="/auth/secret" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Secret</a>
       </div>
     </div>
+    </Layout>
   );
 }
